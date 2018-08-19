c6ec6278b12afcb9517bf32af998815302c488e8
Eman Mohammed <emy_eva23@yahoo.com>
2018-08-19 04:02:15 +0200

    Fech from server

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -6,18 +6,20 @@
 
 
 
-window.addEventListener('load',  (event)=>{
-app.get('/sw.js', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '..', 'build', 'sw.js'));
-});if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
-  .then(function(registration) {
-    console.log('Registration successful, scope is:', registration.scope);
-  })
-  .catch(function(error) {
-    console.log('Service worker registration failed, error:', error);
-  });
-} });                      
+window.addEventListener('load', (event) => {
+    app.get('/sw.js', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '..', 'build', 'sw.js'));
+        });
+        if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js')
+      .then(function(registration) {
+        console.log('Registration successful, scope is:', registration.scope);
+      })
+      .catch(function(error) {
+        console.log('Service worker registration failed, error:', error);
+      });
+    }
+});                      
 
 class DBHelper {
 
@@ -26,34 +28,37 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
-     $.get( "ajax/test.html", function( data ) {
-  $( ".result" ).html( data );
-  alert( "Load was performed." );
-});
+    const port = 1337
+    return `http://localhost:${port}/restaurants`;
   }
 
-    
-    
+    //static fetchRestaurants(callback) {
+    //    fetch(DBHelper.DATABASE_URL, { method: 'GET' })
+    //        .then(response => response.json().then(result => () => {
+    //            console.log(result);
+    //            callback(null, restaurants);
+    //        }));
+    //   // DBHelper.fetchRestaurantsAgain();
+    //}
+
   /**
    * Fetch all restaurants.
    */
-  static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', DBHelper.DATABASE_URL);
-    xhr.onload = () => {
-      if (xhr.status === 200) { // Got a success response from server!
-        const json = JSON.parse(xhr.responseText);
-        const restaurants = json.restaurants;
-        callback(null, restaurants);
-      } else { // Oops!. Got an error from server.
-        const error = (`Request failed. Returned status of ${xhr.status}`);
-        callback(error, null);
-      }
-    };
-    xhr.send();
+    static fetchRestaurants(callback) {
+      fetch(DBHelper.DATABASE_URL, {
+          method: 'GET'
+      }).then(response => response.json().then(function (restaurants) {
+          console.log(restaurants);
+          callback(null, restaurants);
+
+
+       
+
+
+     }));
   }
+
+
 
   /**
    * Fetch a restaurant by its ID.
@@ -174,7 +179,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`/img/${restaurant.photograph}.jpg`);
   }
 
   /**
